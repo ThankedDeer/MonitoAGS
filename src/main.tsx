ba65d80930ffd81f4948c42fdf3e88fffe2b3ed9
ThankedDeer <eduardo.salinas12@hotmail.com>
2024-10-19 03:50:23 -0600

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,28 @@
+import { NextUIProvider } from "@nextui-org/react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <NextUIProvider>
+      <RouterProvider router={router} />
+    </NextUIProvider>
+  </StrictMode>
+);
